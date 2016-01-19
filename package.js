45c9b03147d23be4a7fa273634af3f6d825cc6f4
nicholasbutlin <nicholasbutlin@gmail.com>
2016-01-19 17:19:45 +0000

    updated package info

--- package.js
+++ package.js
@@ -8,7 +8,7 @@ Package.describe({
 Package.onUse(function (api, where) {
   api.versionsFrom('0.9.0');
   api.use(['nickos:errors@1.0.0', 'templating'], 'client');
-  api.use('iron:router')
+  api.use('iron:router@1.0.0')
   api.addFiles(['client_contact.js', 'client_contact.html'], 'client');
   api.addFiles(['libcontact.js', 'libroute.js']);
   api.export('Contact');
