02e656c387547900bb0869a8a4508fb43f825d19
nicholasbutlin <nicholasbutlin@gmail.com>
2016-01-19 18:29:17 +0000

    updated dependencies

--- package.js
+++ package.js
@@ -1,15 +1,14 @@
 Package.describe({
   name: 'nickos:contact',
   summary: "creates a simple contact form",
-  version: "1.0.1",
+  version: "1.0.3",
   git: "https://github.com/nicholasbutlin/meteor-contact.git"
   });
 
 Package.onUse(function (api, where) {
   api.versionsFrom('0.9.0');
-  api.use(['nickos:errors@1.0.0', 'templating'], 'client');
-  api.use('iron:router@1.0.0')
+  api.use(['nickos:errors@1.0.0', 'templating', 'iron:router@1.0.0'], ['client', 'server']);
   api.addFiles(['client_contact.js', 'client_contact.html'], 'client');
-  api.addFiles(['libcontact.js', 'libroute.js']);
-  api.export('Contact');
+  api.addFiles(['libcontact.js', 'libroute.js'], ['client', 'server']);
+  api.export('contact', ['client', 'server']);
 });
